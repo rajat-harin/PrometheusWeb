10999d02d2e9a73f215893eed90b0285dbcdeae4
rajat-harin <rajat.harinkhede@gmail.com>
2021-05-03 17:20:31 +0530

    Added Dependency Injection

--- PrometheusWeb.Services/Controllers/CoursesController.cs
+++ PrometheusWeb.Services/Controllers/CoursesController.cs
@@ -11,81 +11,59 @@ using System.Web.Http.Description;
 using PrometheusWeb.Data;
 using PrometheusWeb.Data.DataModels;
 using PrometheusWeb.Data.UserModels;
+using PrometheusWeb.Services.Services;
 
 namespace PrometheusWeb.Services.Controllers
 {
     public class CoursesController : ApiController
     {
-        private PrometheusEntities db = new PrometheusEntities();
+        private ICourseService _courseService = null;
+        public CoursesController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
 
         // GET: api/Courses
         public IQueryable<CourseUserModel> GetCourses()
         {
 
-            return db.Courses.Select(item => new CourseUserModel
-            {
-                CourseID = item.CourseID,
-                Name = item.Name,
-                StartDate = item.StartDate,
-                EndDate = item.EndDate
-            }) ;
+            return _courseService.GetCourses();
         }
 
         // GET: api/Courses/5
         [ResponseType(typeof(CourseUserModel))]
         public IHttpActionResult GetCourse(int id)
         {
-            Course course = db.Courses.Find(id);
+            CourseUserModel course = _courseService.GetCourse(id);
             if (course == null)
             {
                 return NotFound();
             }
-            CourseUserModel courseUser = new CourseUserModel
-            {
-                CourseID = course.CourseID,
-                Name = course.Name,
-                StartDate = course.StartDate,
-                EndDate = course.EndDate
-            };
-            return Ok(courseUser);
+            return Ok(course);
         }
 
         // PUT: api/Courses/5
         [ResponseType(typeof(void))]
         public IHttpActionResult PutCourse(int id, CourseUserModel courseModel)
         {
-            Course course = new Course
-            {
-                CourseID = courseModel.CourseID,
-                Name = courseModel.Name,
-                StartDate = courseModel.StartDate,
-                EndDate = courseModel.EndDate
-            };
+            bool result;
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
-
-            if (id != course.CourseID)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(course).State = EntityState.Modified;
-
             try
             {
-                db.SaveChanges();
+                result = _courseService.UpdateCourse(id, courseModel);
             }
-            catch (DbUpdateConcurrencyException)
+            catch (Exception)
             {
-                if (!CourseExists(id))
+                if (!_courseService.CourseExists(id))
                 {
                     return NotFound();
                 }
                 else
                 {
-                    throw;
+                    return StatusCode(HttpStatusCode.InternalServerError);
                 }
             }
 
@@ -96,52 +74,23 @@ namespace PrometheusWeb.Services.Controllers
         [ResponseType(typeof(Course))]
         public IHttpActionResult PostCourse(CourseUserModel courseModel)
         {
-            Course course = new Course
-            {
-                CourseID = courseModel.CourseID,
-                Name = courseModel.Name,
-                StartDate = courseModel.StartDate,
-                EndDate = courseModel.EndDate
-            };
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            db.Courses.Add(course);
-            db.SaveChanges();
-
-            return CreatedAtRoute("DefaultApi", new { id = course.CourseID }, course);
+            var result = _courseService.AddCourse(courseModel);
+            if(result)
+                return CreatedAtRoute("DefaultApi", new { id = courseModel.CourseID }, courseModel);
+            else
+                return StatusCode(HttpStatusCode.InternalServerError);
         }
 
         // DELETE: api/Courses/5
         [ResponseType(typeof(Course))]
         public IHttpActionResult DeleteCourse(int id)
         {
-            Course course = db.Courses.Find(id);
-            if (course == null)
-            {
-                return NotFound();
-            }
-
-            db.Courses.Remove(course);
-            db.SaveChanges();
+            CourseUserModel course = _courseService.DeleteCourse(id);
 
             return Ok(course);
         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        
 
-        private bool CourseExists(int id)
-        {
-            return db.Courses.Count(e => e.CourseID == id) > 0;
-        }
     }
 }
