c8f3679064e9337976e716f15c9c60a66b9e4511
sanikaajane <sanikaajane91@gmail.com>
2021-05-05 09:50:48 +0530

    Admin Module Done

--- PrometheusWeb.Data/UserModels/TeacherUserModel.cs
+++ PrometheusWeb.Data/UserModels/TeacherUserModel.cs
@@ -1,26 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PrometheusWeb.Data.UserModels
 {
-    public class AdminUserModel
+    [Table("Teacher")]
+    public class TeacherUserModel
     {
         public int TeacherID { get; set; }
-        public int StudentID { get; set; }
+
+        
         public string FName { get; set; }
+
+        
         public string LName { get; set; }
+
+        
         public string UserID { get; set; }
-        public DateTime? DOB { get; set; }
+
+        
         public string Address { get; set; }
+
+        public DateTime? DOB { get; set; }
+
+        
         public string City { get; set; }
+
+        
         public string MobileNo { get; set; }
-        public bool IsAdmin { get; set; }
-        public string Password { get; set; }
-        public string Role { get; set; }
-        public string SecurityQuestion { get; set; }
-        public string SecurityAnswer { get; set; }
+
+        public bool? IsAdmin { get; set; }
     }
 }
---
+++ PrometheusWeb.Services/Controllers/TeachersController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using PrometheusWeb.Data;
+using PrometheusWeb.Data.DataModels;
+using PrometheusWeb.Data.UserModels;
+
+namespace PrometheusWeb.Services.Controllers
+{
+    public class TeachersController : ApiController
+    {
+        private PrometheusEntities db = new PrometheusEntities();
+
+        // GET: api/Teachers
+        public IQueryable<TeacherUserModel> GetTeachers()
+        {
+            return db.Teachers.Select(item => new TeacherUserModel
+            {
+                TeacherID = item.TeacherID,
+                FName = item.FName,
+                LName = item.LName,
+                UserID = item.UserID,
+                DOB = item.DOB,
+                Address = item.Address,
+                City = item.City,
+                MobileNo = item.MobileNo,
+                IsAdmin = item.IsAdmin
+            });
+        }
+
+        // GET: api/Teachers/5
+        [ResponseType(typeof(Teacher))]
+        public IHttpActionResult GetTeacher(int id)
+        {
+            Teacher teacher = db.Teachers.Find(id);
+            if (teacher == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(teacher);
+        }
+
+        // PUT: api/Teachers/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutTeacher(int id, Teacher teacher)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != teacher.TeacherID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(teacher).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TeacherExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Teachers
+        [ResponseType(typeof(Teacher))]
+        public IHttpActionResult PostTeacher(Teacher teacher)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Teachers.Add(teacher);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = teacher.TeacherID }, teacher);
+        }
+
+        // DELETE: api/Teachers/5
+        [ResponseType(typeof(Teacher))]
+        public IHttpActionResult DeleteTeacher(int id)
+        {
+            Teacher teacher = db.Teachers.Find(id);
+            if (teacher == null)
+            {
+                return NotFound();
+            }
+
+            db.Teachers.Remove(teacher);
+            db.SaveChanges();
+
+            return Ok(teacher);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool TeacherExists(int id)
+        {
+            return db.Teachers.Count(e => e.TeacherID == id) > 0;
+        }
+    }
+}
