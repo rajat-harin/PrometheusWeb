cbf4f38f83cff2ad58ff1a196540b16cec3385aa
Sanket1823 <yosank3@gmail.com>
2021-05-06 19:07:33 +0530

    Added Services Unit Testing and IndexPage

--- PrometheusWeb.UnitTest/Services/AssignmentServiceTests.cs
+++ PrometheusWeb.UnitTest/Services/AssignmentServiceTests.cs
@@ -1,107 +1,94 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using PrometheusWeb.Data.UserModels;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrometheusWeb.Services;
 using PrometheusWeb.Services.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PrometheusWeb.Data.UserModels;
 
-namespace PrometheusWeb.Services.Services.Tests
+namespace PrometheusWeb.UnitTest.Services
 {
-    [TestClass()]
+    [TestClass]
     public class AssignmentServiceTests
     {
-        [TestMethod()]
+        AssignmentService assignmentService = new AssignmentService();
+
+
+        [TestMethod]
         public void GetAssignmentsTest()
         {
-            //Arrange
-            AssignmentService assignmentService = new AssignmentService();
- 
+
             //Act
             var result = assignmentService.GetAssignments();
 
             //Assert
             Assert.IsNotNull(result);
         }
-
-        [TestMethod()]
+        [TestMethod]
         public void GetAssignmentTest()
         {
-            //Arrange
-            AssignmentService assignmentService = new AssignmentService();
-            int id = 1;
+            int Id = 10;
+            //Arrange 
+
             //Act
-            var result = assignmentService.GetAssignment(id);
+            var result = assignmentService.GetAssignment(Id);
 
             //Assert
             Assert.IsNotNull(result);
         }
 
-        [TestMethod()]
+        [TestMethod]
         public void AddAssignmentTest()
         {
             //Arrange
-            AssignmentService assignmentService = new AssignmentService();
-            AssignmentUserModel userModel = new AssignmentUserModel
+            AssignmentUserModel assignmentUserModel = new AssignmentUserModel
             {
                 AssignmentID = 99,
-                CourseID = 1,
                 HomeWorkID = 1,
+                CourseID = 2,
                 TeacherID = 1
+
+
             };
-
-            //Act
-            var result = assignmentService.AddAssignment(userModel);
-
-            //Assert
-            Assert.IsNotNull(result);
+            var result = assignmentService.AddAssignment(assignmentUserModel);
+            Assert.IsTrue(result);
         }
 
-        [TestMethod()]
+        [TestMethod]
         public void UpdateAssignmentTest()
         {
             //Arrange
-            AssignmentService assignmentService = new AssignmentService();
-            AssignmentUserModel userModel = new AssignmentUserModel
+            int id = 10;
+            AssignmentUserModel assignmentUserModel = new AssignmentUserModel
             {
-                AssignmentID = 99,
+                AssignmentID = 10,
+                HomeWorkID = 10,
                 CourseID = 1,
-                HomeWorkID = 1,
                 TeacherID = 1
+
+
             };
-
-            //Act
-            var result = assignmentService.UpdateAssignment(userModel.AssignmentID,userModel);
-
-            //Assert
-            Assert.IsNotNull(result);
+            var result = assignmentService.UpdateAssignment(id, assignmentUserModel);
+            Assert.IsTrue(result);
         }
-
-        [TestMethod()]
+        [TestMethod]
         public void DeleteAssignmentTest()
         {
             //Arrange
-            AssignmentService assignmentService = new AssignmentService();
-            int id = 5;
-
+            int id = 10;
             //Act
-            var result = assignmentService.DeleteAssignment(id);
 
+            var result = assignmentService.DeleteAssignment(id);
             //Assert
             Assert.IsNotNull(result);
         }
 
-        [TestMethod()]
-        public void AssignmentExistsTest()
+        [TestMethod]
+        public void IsAssignmentExistsTest()
         {
             //Arrange
-            AssignmentService assignmentService = new AssignmentService();
-            int id = 2;
-
+            int id = 11;
             //Act
-            var result = assignmentService.IsAssignmentExists(id);
 
+            var result = assignmentService.IsAssignmentExists(id);
             //Assert
             Assert.IsTrue(result);
         }
