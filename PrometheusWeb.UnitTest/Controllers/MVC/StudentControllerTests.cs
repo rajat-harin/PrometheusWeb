d13f3531095497aa179bcb2eef9eceb16f8ff98f
rajat-harin <rajat.harinkhede@gmail.com>
2021-05-06 17:58:29 +0530

    Some Changes On AdminUM

--- PrometheusWeb.UnitTest/Controllers/MVC/StudentControllerTests.cs
+++ PrometheusWeb.UnitTest/Controllers/MVC/StudentControllerTests.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrometheusWeb.Data.UserModels;
 using PrometheusWeb.MVC.Controllers;
+using PrometheusWeb.MVC.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -31,49 +33,134 @@ namespace PrometheusWeb.MVC.Controllers.Tests
         [TestMethod()]
         public void MyCoursesTest()
         {
-            Assert.Fail();
+            //Arrange
+            var controller = new StudentController();
+
+            //Act
+            var result = controller.MyCourses().Result as ViewResult;
+            //Assert
+            Assert.IsNotNull(result); //Testing for Null result
+            Assert.IsInstanceOfType(result, typeof(ViewResult)); // Type Checking
+            Assert.IsInstanceOfType(result.Model, typeof(List<EnrollmentUserModel>)); // Testing if Model type is Correct
+            Assert.IsNotNull(result.Model); //Testing for Null result Model
         }
 
         [TestMethod()]
         public void ViewCoursesTest()
         {
-            Assert.Fail();
+            //Arrange
+            var controller = new StudentController();
+
+            //Act
+            var result = controller.ViewCourses().Result as ViewResult;
+            //Assert
+            Assert.IsNotNull(result); //Testing for Null result
+            Assert.IsInstanceOfType(result, typeof(ViewResult)); // Type Checking
+            Assert.IsInstanceOfType(result.Model, typeof(List<CourseUserModel>)); // Testing if Model type is Correct
+            Assert.IsNotNull(result.Model); //Testing for Null result Model
         }
 
         [TestMethod()]
         public void GetHomeworksTest()
         {
-            Assert.Fail();
+            //Arrange
+            var controller = new StudentController();
+
+            //Act
+            var result = controller.GetHomeworks().Result as ViewResult;
+            //Assert
+            Assert.IsNotNull(result); //Testing for Null result
+            Assert.IsInstanceOfType(result, typeof(ViewResult)); // Type Checking
+            Assert.IsInstanceOfType(result.Model, typeof(List<AssignedHomework>)); // Testing if Model type is Correct
+            Assert.IsNotNull(result.Model); //Testing for Null result Model
         }
 
         [TestMethod()]
         public void EnrollInCourseTest()
         {
-            Assert.Fail();
+            //Arrange
+            var controller = new StudentController();
+
+            //Act
+            var result = controller.EnrollInCourse(new CourseUserModel
+            {
+                CourseID = 1,
+                Name = "Math",
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now
+            }).Result as ViewResult;
+            //Assert
+            Assert.IsNotNull(result); //Testing for Null result
+            Assert.IsInstanceOfType(result, typeof(ViewResult)); // Type Checking
+            Assert.IsNotNull(result.ViewBag); //Testing for Null result Viewbag
+            Assert.IsInstanceOfType(result.ViewBag.Message, typeof(string)); // Testing if Model type is Correct
+            
         }
 
         [TestMethod()]
         public void GetHomeworkPlanTest()
         {
-            Assert.Fail();
+            //Arrange
+            var controller = new StudentController();
+
+            //Act
+            var result = controller.GetHomeworkPlan().Result as ViewResult;
+            //Assert
+            Assert.IsNotNull(result); //Testing for Null result
+            Assert.IsInstanceOfType(result, typeof(ViewResult)); // Type Checking
+            Assert.IsInstanceOfType(result.Model, typeof(List<HomeworkPlanUserModel>)); // Testing if Model type is Correct
+            Assert.IsNotNull(result.Model); //Testing for Null result Model
         }
 
         [TestMethod()]
         public void GeneratePlanTest()
         {
-            Assert.Fail();
+            //Arrange
+            var controller = new StudentController();
+
+            //Act
+            var result = controller.GeneratePlan().Result as ViewResult;
+            //Assert
+            Assert.IsNotNull(result); //Testing for Null result
+            Assert.IsInstanceOfType(result, typeof(ViewResult)); // Type Checking
+            
         }
 
         [TestMethod()]
         public void UpdatePlanTest()
         {
-            Assert.Fail();
+            //Arrange
+            var controller = new StudentController();
+
+            //Act
+            var result = controller.UpdatePlan().Result as ViewResult;
+            //Assert
+            Assert.IsNotNull(result); //Testing for Null result
+            Assert.IsInstanceOfType(result, typeof(ViewResult)); // Type Checking
+            Assert.IsInstanceOfType(result.Model, typeof(HomeworkPlanUserModel)); // Testing if Model type is Correct
+            Assert.IsNotNull(result.Model); //Testing for Null result Model
         }
 
         [TestMethod()]
         public void UpdatePlanTest1()
         {
-            Assert.Fail();
+            //Arrange
+            var controller = new StudentController();
+
+            //Act
+            var result = controller.UpdatePlan(new HomeworkPlanUserModel
+            {
+                HomeworkPlanID = 1,
+                isCompleted = false,
+                StudentID = 1,
+                HomeworkID = 1,
+                PriorityLevel = 1
+            }).Result as ViewResult;
+            //Assert
+            Assert.IsNotNull(result); //Testing for Null result
+            Assert.IsInstanceOfType(result, typeof(ViewResult)); // Type Checking
+            Assert.IsInstanceOfType(result.Model, typeof(HomeworkPlanUserModel)); // Testing if Model type is Correct
+            Assert.IsNotNull(result.Model); //Testing for Null result Model
         }
     }
 }
