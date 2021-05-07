d8eadef8801b3bf7bbc2e4bd5a485844a3aecc2a
rajat-harin <rajat.harinkhede@gmail.com>
2021-05-07 19:39:37 +0530

    Authentication Implemented(Only for Courses)

--- PrometheusWeb.Services/Controllers/CoursesController.cs
+++ PrometheusWeb.Services/Controllers/CoursesController.cs
@@ -24,7 +24,6 @@ namespace PrometheusWeb.Services.Controllers
         }
 
         // GET: api/Courses
-        [Authorize]
         public IQueryable<CourseUserModel> GetCourses()
         {
             return _courseService.GetCourses();
